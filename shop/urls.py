1e736096c93abc4dc81c0f73df5998df85803017
White-Billa <tusharhirapara37372@gmail.com>
2023-10-07 19:22:08 +0530

    changes done
    changes done

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from shop import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "myapp"
+app_name = "shop"
 
 urlpatterns = [
     path("", views.Index, name="index"),
