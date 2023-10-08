72b6d0016b9d47db62e082ac476b8b7fe36cbdac
White-Billa <tusharhirapara37372@gmail.com>
2023-10-08 20:48:27 +0530

    add all filters set limit title characters

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from shop import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,5 +7,5 @@ app_name = "shop"
 
 urlpatterns = [
     path("", views.Index, name="index"),
-    path('products', views.Products,name="products")
+    path("products", views.Products,name="products"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -10,13 +10,60 @@ def Index(request):
     }
     return render(request,'index.html',context)
 def Products(request):
-    product = Product.objects.all(),
     categories=Categories.objects.all()
     color=Color.objects.all()
+    filter_price=Filter_price.objects.all()
+    brands=Brands.objects.all()
+
+
+    CATID = request.GET.get('category')
+    F_PRICEID=request.GET.get('filter_price')
+    COLORID=request.GET.get('color')
+    BRANDSID=request.GET.get('brands')
+    ATOZID=request.GET.get('AtoZ')
+    ZTOAID = request.GET.get('ZtoA')
+    LOWTOHIGH=request.GET.get('lowtohigh')
+    HIGHTOLOW=request.GET.get('hightolow')
+    NEWID=request.GET.get('new')
+    OLDID=request.GET.get('old')
+
+    if CATID:
+        product = Product.objects.filter(categories=CATID,status='Publish')
+        print(CATID)
+    elif F_PRICEID:
+        product = Product.objects.filter(filter_price=F_PRICEID, status='Publish')
+    elif COLORID:
+        product = Product.objects.filter(color=COLORID, status='Publish')
+    elif BRANDSID:
+        product = Product.objects.filter(brand=BRANDSID, status='Publish')
+    elif ATOZID:
+        product = Product.objects.filter(status='Publish').order_by('name')
+    elif ZTOAID:
+        product = Product.objects.filter(status='Publish').order_by('-name')
+    elif LOWTOHIGH:
+        product = Product.objects.filter(status='Publish').order_by('price')
+    elif HIGHTOLOW:
+        product = Product.objects.filter(status='Publish').order_by('-price')
+    elif NEWID:
+        product = Product.objects.filter(condition='New',status='Publish').order_by('-id')
+    elif OLDID:
+        product = Product.objects.filter(condition='Old',status='Publish')
+
+
+
+    else:
+        product = Product.objects.filter(status='Publish')
+        print("else")
+
+
+
+
     context = {
         'product': product,
         'categories': categories,
         'color':color,
+        'filter_price':filter_price,
+        'brands':brands,
     }
     return render(request,'product.html',context)
 
